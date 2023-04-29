b49c9218d9e0a4e02165b1f42435952662e96072
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-04-29 17:51:02 +0900

    refactor メイン部分をコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -1,13 +1,15 @@
-import { Inter } from 'next/font/google'
+import { Bentham, Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export function Headline(props) {
   return (
-    <p>
-      Page Get started by editing&nbsp;
-      <code className={styles.code}>src/pages/{props.page}.js</code>
-    </p>
+    <div>
+      <p>
+        Page Get started by editing&nbsp;
+        {props.children}
+      </p>
+    </div>
   )
 }

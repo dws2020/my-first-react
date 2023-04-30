5b725838229a65a82c60b062f9449c6fcb7a8fb4
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-04-30 12:03:37 +0900

    install tailwindcss

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Header() {
     return (
         <header>
-            <Link href="/" className="font-medium">
+            <Link href="/" className="text-3xl font-medium">
                 Index
             </Link>
             <Link href="/about">

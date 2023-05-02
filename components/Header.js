4e6007842fd70bd6aa013812d79f43a1c43b906d
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-02 20:23:10 +0900

    install tailwind css

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { tv } from 'tailwind-variants';
+
+const header = tv({
+    base: "w-full border-solid divide-slate-700",
+    variants: {
+        
+    }
+
+}
+)
 
 export function Header() {
     return (
-        <header>
+        <header className={header()}>
             <Link href="/" className="text-3xl font-medium">
-                Index
+                <span className="hover:text-blue-700">Index</span>
             </Link>
-            <Link href="/about">
+            <Link href="/about" className="text-3xl font-medium ">
                 About
             </Link>
         </header>

8526ed7947bb2f939fcc85b60abd11a26bb513e9
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-03 11:48:07 +0900

    indent変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import { tv } from 'tailwind-variants';
 
 const header = tv({
-    base: "w-full border-solid divide-slate-700",
-    variants: {
-        
-    }
+  base: "w-full border-solid divide-slate-700",
+  variants: {
+
+  }
 
 }
 )
 
 export function Header() {
-    return (
-        <header className={header()}>
-            <Link href="/" className="text-3xl font-medium">
-                <span className="hover:text-blue-700">Index</span>
-            </Link>
-            <Link href="/about" className="text-3xl font-medium ">
-                About
-            </Link>
-        </header>
-    )
+  return (
+    <header className={header()}>
+      <Link href="/" className="text-3xl font-medium">
+        <span className="hover:text-blue-700">Index</span>
+      </Link>
+      <Link href="/about" className="text-3xl font-medium ">
+        About
+      </Link>
+    </header>
+  )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,56 +1,56 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './Main.module.css'
-import { Headline } from './Headline'
-import { Menus } from './Menus'
+import Image        from 'next/image'
+import { Inter }    from 'next/font/google'
+import styles       from './Main.module.css'
+import { Headline } from 'src/components/Headline'
+import { Menus }    from './Menus'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export function Main(props) {
   return (
     <main className={`${styles.main} ${inter.className}`}>
-    <div className={styles.description}>
-        <Headline 
-            title="Index Page" 
-            page="index" 
-            number={1415} 
-            arr={[6,0,1]} 
-            obj={{key: "value"}}
-            boolean
+      <div className={styles.description}>
+        <Headline
+          title="Index Page"
+          page="index"
+          number={1415}
+          arr={[6, 0, 1]}
+          obj={{ key: "value" }}
+          boolean
         >
-            <code className={styles.code}>src/pages/{props.page}.js</code>
+          <code className={styles.code}>src/pages/{props.page}.js</code>
         </Headline>
-        
+
         <div>
-        <a
+          <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-        >
+          >
             By{' '}
             <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className={styles.vercelLogo}
-            width={100}
-            height={24}
-            priority
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
             />
-        </a>
+          </a>
         </div>
-    </div>
+      </div>
 
-    <div className={styles.center}>
+      <div className={styles.center}>
         <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
         />
-    </div>
-    <Menus />
+      </div>
+      <Menus />
     </main>
   )
 }

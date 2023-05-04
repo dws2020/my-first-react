9b92f292f8825515fcc09078a9f3704bebd54f20
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-04 21:29:17 +0900

    experimental クリックイベントをtest

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,24 @@ import Head       from 'next/head'
 import { Inter }  from 'next/font/google'
 import { Main }   from '@/components/Main'
 import { Header } from '@/components/Header'
+import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const val = "1415";
+  const handleClick = useCallback( (e) => {
+    alert(val);
+  }, []);
+  
   return (
     <div>
       <Head>
         <title>Create Next App!</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>btn</button>
       <Main page={'index'} />
     </div>
   )

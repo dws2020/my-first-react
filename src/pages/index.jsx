db7ec6f4b3dbde87b1537a04d8837c390dffc879
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-05 13:26:33 +0900

    カウントアップボタンを実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head       from 'next/head'
 import { Inter }  from 'next/font/google'
 import { Main }   from '@/components/Main'
 import { Header } from '@/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { tv } from 'tailwind-variants'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,22 +12,21 @@ const center = tv({
 });
 
 export default function Home() {
-  const val = "1415";
-  const handleClick = useCallback( (e) => {
-    alert(val);
-  }, []);
-  
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+  };
+
   useEffect(() => {
     // mount時の処理
-    console.log('mount');
     document.body.style.backgroundColor = 'lightblue';
   
     return(() => {
       // unmount時の処理
-      console.log('unmount');
       document.body.style.backgroundColor = '';
     });
   }, []);
+
+  const [count, setCount] = useState(1);
 
   return (
     <div>
@@ -35,8 +34,8 @@ export default function Home() {
         <title>Create Next App!</title>
       </Head>
       <Header />
-      <h1 className={center()}>1</h1>
-      <p className={center()} >
+      <h1 className={center()}>{count}</h1>
+      <p className={center()}>
         <button onClick={handleClick}>btn</button>
       </p>
       <Main page={'index'} />

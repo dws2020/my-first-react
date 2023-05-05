8215be5f73e4c2cd647ad1f4ccdb15b3ce5e32e3
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-05 10:24:24 +0900

    useEffectを実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head       from 'next/head'
 import { Inter }  from 'next/font/google'
 import { Main }   from '@/components/Main'
 import { Header } from '@/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +11,18 @@ export default function Home() {
   const val = "1415";
   const handleClick = useCallback( (e) => {
     alert(val);
+  }, []);
+  
+  useEffect(() => {
+    // mount時の処理
+    console.log('mount');
+    document.body.style.backgroundColor = 'lightblue';
+  
+    return(() => {
+      // unmount時の処理
+      console.log('unmount');
+      document.body.style.backgroundColor = '';
+    });
   }, []);
   
   return (

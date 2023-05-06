e97d6ecccfafdaa45172ac18bce6ba91f2719437
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-06 17:55:57 +0900

    useCallbackの第二引数を指定

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head       from 'next/head'
-import { Inter }  from 'next/font/google'
+import { Inter, Newsreader }  from 'next/font/google'
 import { Main }   from '@/components/Main'
 import { Header } from '@/components/Header'
 import { useCallback, useEffect, useState } from 'react'
@@ -15,6 +15,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [isShow, setIsShow] = useState(true);
   const [inputText, setInputText] = useState('');
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -24,7 +25,7 @@ export default function Home() {
 
   const handleDisplay = useCallback(() => {
     setIsShow(isShow => !isShow);
-  });
+  }, []);
 
   const handleInput = useCallback((e) => {
     if (e.target.value.length > 4) {
@@ -32,7 +33,17 @@ export default function Home() {
       return
     }
     setInputText(e.target.value);
-  });
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray(prevArray => {
+      if (prevArray.some(item => item === inputText)) {
+        alert ('this word is already used');
+        return prevArray; //しまぶーさんは、なぜここにさらっとprevArrayを入れるのか？　入れないとエラーがループ？してしまう。
+      }
+      return [...prevArray, inputText];
+    });
+  }, [inputText]);
 
   useEffect(() => {
     // mount時の処理
@@ -58,6 +69,14 @@ export default function Home() {
         <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
       </p>
       <input type='text' className={`${center()} w-52`} value={inputText} onChange={handleInput} />
+      <button className={center()} onClick={handleAdd}>Add</button>
+      <ul className={center()}>
+        {array.map(li => {
+          return (
+            <li key={li}>{li}</li>
+          )
+        })}
+      </ul>
       <Main page={'index'} />
     </div>
   )

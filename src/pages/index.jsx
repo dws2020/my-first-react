8c9cb395ab1cac80aac9225d4318d1e9147b631a
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-07 18:45:10 +0900

    prettier formatted

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,44 +1,44 @@
-import Head       from 'next/head'
-import { Inter, Newsreader }  from 'next/font/google'
-import { Main }   from '@/components/Main'
-import { Header } from '@/components/Header'
-import { useCallback, useEffect, useState } from 'react'
-import { tv } from 'tailwind-variants'
+import Head from "next/head";
+import { Inter, Newsreader } from "next/font/google";
+import { Main } from "@/components/Main";
+import { Header } from "@/components/Header";
+import { useCallback, useEffect, useState } from "react";
+import { tv } from "tailwind-variants";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const center = tv({
-  base: 'container mx-auto text-center block'
+  base: "container mx-auto text-center block",
 });
 
 export default function Home() {
   const [count, setCount] = useState(1);
   const [isShow, setIsShow] = useState(true);
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount(count => count + 1);
+      setCount((count) => count + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow(isShow => !isShow);
+    setIsShow((isShow) => !isShow);
   }, []);
 
   const handleInput = useCallback((e) => {
     if (e.target.value.length > 4) {
-      alert('less 5 letters, please');
-      return
+      alert("less 5 letters, please");
+      return;
     }
     setInputText(e.target.value);
   }, []);
 
   const handleAdd = useCallback(() => {
-    setArray(prevArray => {
-      if (prevArray.some(item => item === inputText)) {
-        alert ('this word is already used');
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === inputText)) {
+        alert("this word is already used");
         return prevArray; //しまぶーさんは、なぜここにさらっとprevArrayを入れるのか？　入れないとエラーがループ？してしまう。
       }
       return [...prevArray, inputText];
@@ -47,12 +47,12 @@ export default function Home() {
 
   useEffect(() => {
     // mount時の処理
-    document.body.style.backgroundColor = 'lightblue';
-  
-    return(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
       // unmount時の処理
-      document.body.style.backgroundColor = '';
-    });
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
@@ -61,23 +61,28 @@ export default function Home() {
         <title>Create Next App!</title>
       </Head>
       <Header />
-      {isShow ? <h1 className={center()}>{count}</h1> : null }
+      {isShow ? <h1 className={center()}>{count}</h1> : null}
       <p className={center()}>
         <button onClick={handleClick}>btn</button>
       </p>
       <p className={center()}>
-        <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       </p>
-      <input type='text' className={`${center()} w-52`} value={inputText} onChange={handleInput} />
-      <button className={center()} onClick={handleAdd}>Add</button>
+      <input
+        type="text"
+        className={`${center()} w-52`}
+        value={inputText}
+        onChange={handleInput}
+      />
+      <button className={center()} onClick={handleAdd}>
+        Add
+      </button>
       <ul className={center()}>
-        {array.map(li => {
-          return (
-            <li key={li}>{li}</li>
-          )
+        {array.map((li) => {
+          return <li key={li}>{li}</li>;
         })}
       </ul>
-      <Main page={'index'} />
+      <Main page={"index"} />
     </div>
-  )
+  );
 }
